29a15ec4a2cfd4e916e2b2bfc9e07a79cf5bca71
shanebe <shane@shanebennett.ca>
2015-04-07 14:40:32 -0400

    Update media.py

--- media.py
+++ media.py
@@ -3,12 +3,21 @@ import webbrowser
 
 class Movie():
     """ The Movie Class - Contains the blueprint for Movie instances.
-        Usage: Movie('Movie Title','Storyline','Poster Image URL', 'Youtube Trailer URL', 'Rating 0-3 (G,PG,PG13,R)' """
-
-    VALID_RATINGS = ["G","PG","PG-13","R"]
-    #Constant Class Variable "VALID_RATINGS", capitalized as Class Variable
     
-    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube,movie_rating):
+        Attributes: 
+        movie_title: A string containing the movie's title
+        movie_storyline: A string containing the movie's synopsis
+        poster_image: A string containing the URL of the movie poster image file
+        trailer_youtube: A string containing the URL of the movie's trailer on youtube
+        movie_rating: An integer of the movie's rating (Class variable VALID_RATINGS)
+        
+        Usage: Movie('Movie Title', 'Storyline', 'Poster Image URL', 'Youtube Trailer URL', 'Rating 0-3 (G, PG, PG13, R)' """
+
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    # Constant Class Variable "VALID_RATINGS", capitalized as Class Variable
+    
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, movie_rating):
+        """ Inits the Movie class with 5 attributes """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -16,6 +25,5 @@ class Movie():
         self.movie_rating = movie_rating
 
     def show_trailer(self):
-        ''' Shows the trailer for the Movie object '''
+        """ Shows the trailer for the Movie object """
         webbrowser.open(self.trailer_youtube_url)
-
